39ed082d1c9722bde80c09e3435d9eb616e9aecc
James Cape <james.cape@gmail.com>
2020-03-01 15:46:44 -0700

    Adds app components with addTask, toggle, removeTask, and get tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { TaskDataService } from './task-data.service';
+import { Task } from './task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[TaskDataService]
 })
+
 export class AppComponent {
   title = 'task-list-frontend';
+  newTask: Task = new Task();
+
+  constructor(private TaskDataService: TaskDataService) {
+  }
+
+  addTask() {
+    this.TaskDataService.addTask(this.newTask);
+    this.newTask = new Task();
+  }
+
+  toggleTaskCompleted(task) {
+    this.TaskDataService.toggleTaskCompleted(task);
+  }
+
+  removeTask(task) {
+    this.TaskDataService.deleteTaskById(task.id);
+  }
+
+  get tasks() {
+    return this.TaskDataService.getAllTasks();
+  }
 }
